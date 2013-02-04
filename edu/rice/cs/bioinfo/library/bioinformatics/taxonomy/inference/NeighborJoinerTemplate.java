ff9629994bcaaae651e120277a2ad88aaec7fbad
dw20 <dw20@94e8b07c-332e-4ac0-aa82-9412fb3d5b0f>
2013-02-04 22:26:16 +0000

    Rejected commit: Default

--- edu/rice/cs/bioinfo/library/bioinformatics/taxonomy/inference/NeighborJoinerTemplate.java
+++ edu/rice/cs/bioinfo/library/bioinformatics/taxonomy/inference/NeighborJoinerTemplate.java
@@ -1,18 +1,15 @@
 package edu.rice.cs.bioinfo.library.bioinformatics.taxonomy.inference;
-import com.sun.xml.internal.xsom.impl.scd.Axis;
-import edu.rice.cs.bioinfo.library.programming.Tuple;
+
 import java.util.*;
 
 public abstract class NeighborJoinerTemplate<N,E,G,D extends Comparable<D>> implements NeighborJoiner<N,G>
 {
-    public D MAX;
-
     public G performJoin(Set<N> taxa)
     {
-        G resultTree = makeEmptyGraph();
+        G resultTree = makeEdgelessGraph(taxa);
         /* create a linked hash set to store the nodes */
-        Set<N> Node = new HashSet<N>();
-        Node.addAll(taxa);
+        Set<N> node = new HashSet<N>();
+        node.addAll(taxa);
         /* create a edge-distance map to store the weights between nodes */
         Map<N,Map<N,D>> nodeToNodeDistances = new HashMap<N, Map<N, D>>();
         for(N taxon1 : taxa)
@@ -21,50 +18,52 @@ public abstract class NeighborJoinerTemplate<N,E,G,D extends Comparable<D>> impl
             nodeToNodeDistances.put(taxon1, taxon1NodeDistances);
             for(N taxon2 : taxa)
             {
-                D distanceFromTaxon1ToTaxon2 = getDistance(taxon1, taxon2);
-                taxon1NodeDistances.put(taxon2, distanceFromTaxon1ToTaxon2);
+                if(taxon1 != taxon2){
+                    D distanceFromTaxon1ToTaxon2 = getDistance(taxon1, taxon2);
+                    taxon1NodeDistances.put(taxon2, distanceFromTaxon1ToTaxon2);
+                }
             }
         }
-
-        while(Node.size() > 1 ) {
+        while(node.size() > 1 ) {
             /* find the minimum distance and the corresponding nodes from the edge-node map */
-            D temp = MAX;
-            N node1 = makeNewNodeInGraph(resultTree);
-            N node2 = makeNewNodeInGraph(resultTree);
+            Iterator<N> it = node.iterator();
+            N node1 = it.next();
+            N node2 = it.next();
+            D temp = nodeToNodeDistances.get(node1).get(node2);
 
-            for(N a: Node){
-                for(N b: Node){
-                    D distab = nodeToNodeDistances.get(a).get(b);
-                    if(temp.compareTo(distab) > 0){
-                        temp = distab;
-                        node1 = a;
-                        node2 = b;
+            for(N a: node){
+                for(N b: node){
+                    if(a != b){
+                        D distab = nodeToNodeDistances.get(a).get(b);
+                        if(temp.compareTo(distab) > 0){
+                            temp = distab;
+                            node1 = a;
+                            node2 = b;
+                        }
                     }
                 }
             }
             /* delete node1 and node2 from Set */
-            Node.remove(node1);
-            Node.remove(node2);
+            node.remove(node1);
+            node.remove(node2);
            /* calculate and reset the distances from node3 to other node in the Set except node1 and node2 */
             N node3 = makeNewNodeInGraph(resultTree);
-            for(N a: Node){
+            Map<N,D> node3NodeDistances = new HashMap<N, D>();
+            nodeToNodeDistances.put(node3, node3NodeDistances);
+            for(N a: node){
                 D ave = divide(add(nodeToNodeDistances.get(node1).get(a), nodeToNodeDistances.get(node2).get(a)), makeD(2));
-                Map<N, D> node3NodeDistance = new HashMap<N, D>();
-                nodeToNodeDistances.put(node3, node3NodeDistance);
-                node3NodeDistance.put(a, ave);
-                Map<N, D> aNodeDistance = new HashMap<N, D>();
-                nodeToNodeDistances.put(a, aNodeDistance);
-                aNodeDistance.put(node3, ave);
+                nodeToNodeDistances.get(node3).put(a, ave);
+                nodeToNodeDistances.get(a).put(node3, ave);
             }
             /* delete the distances related to node1 and node2 from map */
             nodeToNodeDistances.remove(node1);
             nodeToNodeDistances.remove(node2);
-            for(N a: Node){
+            for(N a: node){
                 nodeToNodeDistances.get(a).remove(node1);
                 nodeToNodeDistances.get(a).remove(node2);
             }
             /* add new node to Set node */
-            Node.add(node3);
+            node.add(node3);
             /* add new edge to Graph */
             addEdgeToGraph(node1, node3, resultTree);
             addEdgeToGraph(node2, node3, resultTree);
@@ -72,16 +71,6 @@ public abstract class NeighborJoinerTemplate<N,E,G,D extends Comparable<D>> impl
 
        return resultTree;
     }
-
-    /***
-     * Returns the distance between two distinct taxa.
-     *
-     * @param taxon1 a taxon
-     * @param taxon2 another taxon
-     * @param dist
-     * @return the distance between teh two taxa.
-     */
-    protected abstract D setDistance(N taxon1, N taxon2, D dist);
 
 
     /***
@@ -94,11 +83,13 @@ public abstract class NeighborJoinerTemplate<N,E,G,D extends Comparable<D>> impl
     protected abstract D getDistance(N taxon1, N taxon2);
 
     /**
-     * Makes a new graph of type G without any member nodes or edges.
+     * Makes a new graph of type G containing the given nodes.
      *
-     * @return an empty graph.
+     * Returned graph will have no edges.
+     *
+     * @return an an edgeless graph containing the given nodes.
      */
-    protected abstract G makeEmptyGraph();
+    protected abstract G makeEdgelessGraph(Set<N> nodes);
 
     /**
      * Creates a new node and adds it to the given graph.
@@ -148,7 +139,7 @@ public abstract class NeighborJoinerTemplate<N,E,G,D extends Comparable<D>> impl
      *
      * @param minuend  a value
      * @param subtrahend a value
-     * @return
+     * @return the substract of the given values
      */
     protected abstract D subtract(D minuend, D subtrahend);
 

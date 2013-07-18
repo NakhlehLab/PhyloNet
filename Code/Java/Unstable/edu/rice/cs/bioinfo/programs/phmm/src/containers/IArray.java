8bff003352826256d645f89ac97e8b3028d80697
kt10 <kt10@94e8b07c-332e-4ac0-aa82-9412fb3d5b0f>
2013-07-18 20:14:27 +0000

    Deleting improperly placed source files.

--- Code/Java/Unstable/edu/rice/cs/bioinfo/programs/phmm/src/containers/IArray.java
+++
@@ -1,7 +0,0 @@
-package containers;
-
-public interface IArray<Type> {
-	public Type get(int index);
-	public void set(int index, Type type);
-	public int getLength();
-}
--- Code/Java/Unstable/edu/rice/cs/bioinfo/programs/phmm/src/containers/IArrayFactory.java
+++
@@ -1,5 +0,0 @@
-package containers;
-
-public interface IArrayFactory<Type> {
-	public IArray<Type> make(int length);
-}

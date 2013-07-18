b8355824439f2ab76eae677eba95de9773f8e068
jd16 <jd16@94e8b07c-332e-4ac0-aa82-9412fb3d5b0f>
2013-07-18 20:57:14 +0000

    re-added source files from backup

---
+++ Code/Java/Unstable/edu/rice/cs/bioinfo/programs/phmm/src/be/ac/ulg/montefiore/run/jahmm/ViterbiCalculator.java
@@ -0,0 +1,246 @@
+/* jahmm package - v0.6.1 */
+
+/*
+  *  Copyright (c) 2004-2006, Jean-Marc Francois.
+ *
+ *  This file is part of Jahmm.
+ *  Jahmm is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Jahmm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Jahmm; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ */
+
+package be.ac.ulg.montefiore.run.jahmm;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import runHmm.MemoryReport;
+import containers.IArrayFactory;
+import containers.IntArrayFactory;
+import containers.ListOfArrays;
+
+
+/**
+ * This class can be used to compute the most probable state sequence matching
+ * a given observation sequence (given an HMM).
+ */
+public class ViterbiCalculator
+{	
+	/*
+	 * The psy and delta values, as described in Rabiner and Juand classical
+	 * papers.
+	 */
+	private double[][] delta;
+	private List<ListOfArrays<Integer>> Psy;
+	private List<Integer> StateSequence;
+	private double lnProbability;
+	
+	
+	/**
+	 * Computes the most likely state sequence matching an observation
+	 * sequence given an HMM.
+	 *
+	 * @param hmm A Hidden Markov Model;
+	 * @param oseq An observations sequence.
+	 */
+	public <O extends Observation> 
+	ViterbiCalculator(List<? extends O> oseq, Hmm<O> hmm)
+	{
+		if (oseq.isEmpty())
+			throw new IllegalArgumentException("Invalid empty sequence");
+		
+		final int growthSize = 10000;
+		
+		IArrayFactory<Integer> arrayFactory = new IntArrayFactory();
+		delta = new double[2][hmm.nbStates()];
+		Psy = new ArrayList<ListOfArrays<Integer>>();
+		StateSequence = new ListOfArrays<Integer>(arrayFactory, growthSize);
+		
+		//building Psy array
+		System.out.println("Building Psy Array");
+		for (int i = 0; i < oseq.size(); i++) {
+		    // kliu - added Integer type argument to fix compilation error
+			Psy.add(new ListOfArrays<Integer>(arrayFactory, hmm.nbStates()));
+			if ((i%10000) == 0) {
+				System.out.print("\r"+ (((double)i/(double)oseq.size()) * 100.0) + "%");
+			}
+		}
+		
+		System.out.print("\r100% Done!                             ");
+		System.out.println("");
+		
+		MemoryReport.report();
+		
+		for (int i = 0; i < hmm.nbStates(); i++) {
+			delta[0][i] = -Math.log(hmm.getPi(i)) - 
+					Math.log(hmm.getOpdf(i).probability(oseq.get(0)));
+			Psy.get(0).add(i, 0);
+		}
+		
+		System.out.println("Begin computing Viterbi's algorithm : FORWARD PART ");
+		Iterator<? extends O> oseqIterator = oseq.iterator();
+		if (oseqIterator.hasNext())
+			oseqIterator.next();
+		
+		int t = 1;
+		while (oseqIterator.hasNext()) {
+			O observation = oseqIterator.next();
+			
+			for (int i = 0; i < hmm.nbStates(); i++) {
+						
+				computeStep(hmm, observation, t, i);
+
+			}
+			for (int k = 0; k < delta[0].length; k++) {
+				delta[0][k] = delta[1][k];
+			}
+			
+			if ((t%10000) == 0) {
+				System.out.print("\r"+ (((double)t/(double)oseq.size()) * 100.0) + "%");
+			}
+			
+			t++;
+			
+		}
+		
+		System.out.print("\r 100% DONE!                                \n");
+		
+		lnProbability = Double.MAX_VALUE;
+		
+		int index = -1;
+		for (int i = 0; i < hmm.nbStates(); i++) {
+			double thisProbability = delta[0][i];
+			
+			if (lnProbability > thisProbability) {
+				lnProbability = thisProbability;
+				index = i;
+			}
+		}
+		StateSequence.add(index);
+		
+		lnProbability = -lnProbability;
+
+		
+		System.out.println("Begin computing Viterbi's algorithm : BACKWARD PART ");
+		int ss = 0; 
+		for (int t2 = oseq.size() - 2; t2 >= 0; t2--) {
+			StateSequence.add(Psy.get(t2+1).get(StateSequence.get(ss)));
+			
+			if ((ss%10000) == 0) {
+				System.out.print("\r"+ (((double)ss/(double)oseq.size()) * 100.0) + "%");
+			}
+			
+			ss++;
+		}
+		
+		System.out.print("\r 100% DONE!                                   \n");
+		
+		
+	}
+	
+	
+	/*
+	 * Computes delta and psy[t][j] (t > 0) 
+	 */
+	private <O extends Observation> void
+	computeStep(Hmm<O> hmm, O o, int t, int j) 
+	{
+		double minDelta = Double.MAX_VALUE;
+		int min_psy = 0;
+		
+		for (int i = 0; i < hmm.nbStates(); i++) {
+			double thisDelta = delta[0][i] - Math.log(hmm.getAij(i, j));
+			
+			
+			if (minDelta > thisDelta) {
+				minDelta = thisDelta;
+				min_psy = i;
+			}
+		}
+		
+		
+		delta[1][j] = minDelta - Math.log(hmm.getOpdf(j).probability(o));
+		Psy.get(t).add(min_psy);
+		
+	}
+	
+	
+	/**
+	 * Returns the neperian logarithm of the probability of the given
+	 * observation sequence on the most likely state sequence of the given
+	 * HMM.
+	 *
+	 * @return <code>ln(P[O,S|H])</code> where <code>O</code> is the given
+	 *         observation sequence, <code>H</code> the given HMM and 
+	 *         <code>S</code> the most likely state sequence of this observation
+	 *         sequence given this HMM.
+	 */
+	public double lnProbability()
+	{
+		return lnProbability;
+	}
+	
+	
+	/**
+	 * Returns a (clone of) the array containing the computed most likely
+	 * state sequence.
+	 *
+	 * @return The state sequence; the i-th value of the array is the index
+	 *         of the i-th state of the state sequence.
+	 */
+	public int[] stateSequence() 
+	{
+		int[] statesseq = new int[StateSequence.size()];
+		int j = 0;
+		for (int i = StateSequence.size() - 1; i >= 0; i--) {
+			statesseq[j] = StateSequence.get(i);
+			j++;
+		}
+		return statesseq;
+	}
+	
+	/**
+	 * Print state sequence out to a file
+	 * 
+	 */
+	public void printStateSequence(String filename) {
+		System.out.println("Begin Printing State Sequence to File.");
+		try {
+		    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(filename, true)));
+		    for (int j = StateSequence.size() -1; j >= 0; j--) {
+		    	out.print(StateSequence.get(j));
+		    	
+		    	if (j % 1024 == 0) {
+		    		out.flush();
+		    	}
+		    	
+		    	if ((j %10000) == 0) {
+					System.out.print("\r"+ (((double)j/(double)StateSequence.size()) * 100.0) + "%");
+				}
+		    }
+		    
+		    System.out.print("\r 100% DONE!                                \n");
+		    
+		    out.close();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	}
+	
+}
---
+++ Code/Java/Unstable/edu/rice/cs/bioinfo/programs/phmm/src/runHmm/runHmm.java
@@ -0,0 +1,411 @@
+package runHmm;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import phylogeny.EvoTree;
+import phylogeny.TreeParser;
+import reader.Parser;
+import reader.ParserFileException;
+import be.ac.ulg.montefiore.run.jahmm.Hmm;
+import be.ac.ulg.montefiore.run.jahmm.MyHMM;
+import be.ac.ulg.montefiore.run.jahmm.ObservationInteger;
+
+public class runHmm {
+
+	private static final double tolerated_error = 0.02;		/* Sum of probabilities margin of error allowed */
+	
+	// stored information 
+	private static String basicFileName = null;				/* Basic Info file name */
+	private static String treesFileName = null;				/* Trees file name */
+			
+	
+	// information created/built
+	public static ArrayList<EvoTree> trees_states;			/* List of all states/trees */
+	public static Hmm<ObservationInteger> myhmm;			/* The entire HMM */
+	public static double[] pi;								/* The initial pi probabilities for each state */
+	public static double[][] aij;							/* The state transition probability matrix */
+	private static Parser fParser;							/* The parser for all basic info and read sequences --> also calculates likelihood */
+	private static int numStates = -1;						/* The number of states for the HMM */
+	
+	/**
+	 * @param args
+	 * @throws Exception
+	 * 		- will only throw exceptions when basic Info file or Tree file is unable to be parsed correctly
+	 * 			since these files are essential to building an hmm and the parser
+	 */
+	public static void main(String[] args) throws Exception{
+		
+		int option;
+	
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		option = initial(in);
+		switch (option) {
+		case 0: 
+			// Get the basic Info File
+			System.out.println("Input the basic file info path name:\n (note: see README for file format) \n ");
+			basicFileName = in.readLine();
+			
+			// Get Trees/States Information
+			boolean getNumStates = true;
+			while (getNumStates) {
+				System.out.println("Input the number of trees or states for this HMM:");
+				try {
+					numStates = Integer.parseInt(in.readLine());
+					getNumStates = false;
+				} catch (NumberFormatException e) {
+					System.out.println( e + "\nError: Not a number! Try again!");
+				}
+			}
+			
+			System.out.println("\nInput the trees (states) file path name:\n (note: see README for file format) \n");
+			treesFileName = in.readLine();
+			
+			// Get Pi probabilities array
+			getPiInfo(in);
+			
+			// Get transition Matrix
+			getAij(in);
+			
+			// Now build the trees and get the tree mapping to integers
+			buildTrees();
+			
+			//Reading in Basic Info file and store information
+			buildParser();
+			
+			//Build HMM
+			buildHMM();
+			
+			break;
+		
+		case 1: 
+			System.out.println("Get existing model");
+			break;
+		
+		case 2: 
+			System.exit(0);
+			
+		default:
+			System.exit(-1);
+		}
+		
+		
+		
+		//Operate mode
+		boolean keepOperate = true;
+		while (keepOperate) {
+			option = operate(in);
+			switch(option) {
+			case 0:
+				System.out.print("\n");
+				// get name of output file
+				System.out.println("Path to your output file: ");
+				String outputfile = in.readLine();
+				
+				ArrayList<ObservationInteger> obsSeq = getObsForViterbi(in);
+				myhmm.saveMostLikelyStateSequence(obsSeq, outputfile);
+				
+				break;
+			case 1:
+				keepOperate = false;
+				System.exit(0);
+				break;
+			default: 
+				keepOperate = false;
+				System.exit(-1);
+			}
+		}
+	}
+	
+	
+	/**
+	 * Initial step and options:
+	 * a. build new model
+	 * b. load a pre-existing model
+	 * c. exit
+	 * 
+	 * @return the option
+	 */
+	static private int initial(BufferedReader in) {
+		boolean init = true;
+		int option = -1;
+		while (init) {
+			System.out.println("Initial mode:");
+			System.out.println("0) Build a new model.");
+			System.out.println("1) Load a pre-existing model.");
+			System.out.println("2) Exit.");
+			System.out.println("Choose an option: ");
+			option = getOption(3, in);
+			if (option != -1) init = false;
+		}
+		return option;
+	}
+	
+	
+	
+	/**
+	 * Operate on Model:
+	 * a. Read in sequence files
+	 * b. Use Viterbi's on existing model
+	 * c. exit
+	 * 
+	 * @return the option
+	 */
+	private static int operate(BufferedReader in) {
+		boolean operate = true;
+		int option = -1;
+		while (operate) {
+			System.out.println("Operate mode:");
+			System.out.println("0) Use Viterbi's on an observation sequence");
+			System.out.println("1) Exit.");
+			System.out.println("Choose an option: ");
+			option = getOption(3, in);
+			if (option != -1) operate = false;
+		}
+		return option;
+	}
+	
+	
+
+	
+
+	/**
+	 * Get Observation for Viterbi
+	 */
+	 private static ArrayList<ObservationInteger> getObsForViterbi(BufferedReader in) {
+			try {
+				System.out.println("Input observation sequence file path name : ");
+				String filename = in.readLine();
+				fParser.parseMe(filename, myhmm);
+				return fParser.getObs();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		
+	}
+	
+	
+	/**
+	 * Helper function
+	 * Given the number of options, this method will read in the user
+	 * choice while catching illegal inputs and output the user choice option
+	 * 
+	 * @param numOptions int - the number options available
+	 * @param option int - the option the user inputted
+	 * @return int the option number
+	 */
+	private static int getOption(int numOptions, BufferedReader in) {
+		try {
+			int option = Integer.parseInt(in.readLine());
+			if ((option >= 0) && (option < numOptions)) {
+				return option;
+			}
+			else {
+				System.out.println("Your answer is not an option!");
+				return -1;
+			}
+		} catch (NumberFormatException e) {
+			System.out.print("Error: not a number!");
+			return -1;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
+	
+	
+	/**
+	 * Build HMM
+	 */
+	private static void buildHMM() {
+		System.out.println("\n\nNow building HMM . . .");
+		myhmm = MyHMM.buildMyHmm(pi, aij, (int) Math.pow(fParser.getAlphabet().size(),fParser.getNumSeq()));
+		System.out.println("\nhey my hmm: \n " + myhmm);
+	}
+	
+	
+	/**
+	 * Read and parse basic info file
+	 * to build the Parser for reading sequences
+	 * @throws Exception 
+	 */
+	private static void buildParser() throws Exception {
+		if(basicFileName != null) {
+			System.out.println("\nNow reading and saving Basic Info for parser . . .");
+			fParser = new Parser(basicFileName);
+			fParser.setTrees(trees_states);
+		} else throw new ParserFileException("Cannot read Basic Info File!");
+		
+	}
+	
+	
+	/**
+	 * Read and parse trees file
+	 * @throws Exception 
+	 */
+	private static void buildTrees() throws Exception {
+		if (treesFileName != null) {
+			System.out.println("\nNow building trees . . .");
+			BufferedReader treesbr = new BufferedReader(new FileReader(treesFileName));
+	        TreeParser tp = new TreeParser(treesbr);
+	        trees_states = tp.nexusFileTreeNames(treesFileName);
+	        
+	        if (numStates != trees_states.size()) {
+	        	treesbr.close();
+	        	throw new ParserFileException("Error: the number of trees read is not the same as the number of states previously inputted!");
+	        }
+	        treesbr.close();
+	        
+	        // ------- >Testing purposes
+//	        System.out.println("Trees read in and built:");
+//	        for (int i = 0; i < trees_states.size(); i++) {
+//	        	System.out.println((trees_states.get(i)));
+//	        }
+	        // <------Testing purposes
+		} else throw new ParserFileException("Cannot read Trees file!");
+	}
+	
+	
+	
+	
+	/**
+	 * Get State Transition Matrix Aij
+	 * @throws IOException - rare
+	 */
+	private static void getAij(BufferedReader in) throws IOException {
+		boolean getA = true;
+		boolean getChoice = true;
+		
+		while (getA) {			
+			// Getting transition matrix A
+			System.out.println("\nThe Transition of States/Trees Matrix Aij. \n(Note: Make sure the order in the matrix correspond to the same order of trees in the given tree input file.");
+			while (getChoice) {
+				System.out.println("Choose an option: \n(a) Read transition matrix by file.\n(b) Input transition matrix manually.");
+				String choice = in.readLine().toLowerCase();
+				if (choice.equals("a")) {
+					try {
+						System.out.println("Please see README for .matrix file format.");
+						System.out.println("Input .matrix file path name:");
+						String filename = in.readLine();
+						BufferedReader filebr = new BufferedReader(new FileReader(filename));
+						aij = readAij(numStates, filebr);
+						filebr.close();
+						getChoice = false;
+					} catch (Exception e) {
+						System.out.println(e);
+						System.out.println("Please try again. \n");
+					}
+				}
+				else if (choice.equals("b")) {
+					try {
+						System.out.println("Example transition for 3 trees (inputted 3 times): \n   .3 .2 .5 \n   .2 .4 .4 \n   .5 .4 .1 \n");
+						System.out.println("Input transition Aij matrix:");
+						aij = readAij(numStates, in);
+						getChoice = false;
+					} catch (Exception e) {
+						System.out.println(e);
+						System.out.println("Please try again.");
+					}
+				}
+			}
+			getA = false;
+		}
+	}
+	
+	
+	
+	/**
+	 * Get Initial Pi Probabilities array
+	 * @throws IOException - rare
+	 */
+	private static void getPiInfo(BufferedReader in) throws IOException {
+		boolean getPi = true;
+		
+		while (getPi) {
+			
+				// Getting pi
+				System.out.println("\nThe Pi or Initial Probabilities. \n(Note: Make sure pi probabilities correspond to the same order of trees in the trees input file.) \n Sample input for 4 trees/states: .2 .3 .1 .4");
+				System.out.println("Input Initial Pi probabilities:");
+				String[] piString;
+				piString = in.readLine().split(" ");
+
+				
+				// Error Check: check to see if number of states and number of pi probabilities are the same
+				if (numStates == piString.length) {
+					getPi = false;
+					
+					// make pi probabilities array
+					pi = new double[numStates];
+					double sum = 0;
+					for (int i=0; i < piString.length; i++) {
+						try {
+							pi[i] = Double.parseDouble(piString[i]);
+							sum += pi[i];
+						} 
+						catch (NumberFormatException e) { 
+							getPi = true;
+							System.out.println("Number format error: Cannot convert inputed pi number : " + piString[i] + " to a double."); 
+						}
+					}
+					if (Math.abs(1.0 - sum) > tolerated_error) {
+						System.out.println("Error: the sum of the pi probabilities did not sum up to 1.0");
+						getPi = true;
+					}
+					
+				} else System.out.println("Error: number of pi probability inputs did not match number of states! ");
+
+		}
+	}
+	
+	
+	
+	/**
+	 * Read in Transition matrix
+	 * @param numStates - the number of states/trees
+	 * @param br - a buffered reader
+	 * @returns a double[][] transition of states/trees matrix probabilities
+	 * @throws Exception - Either an IO exception if an IO error occurred or a ParserFileException that signals an incorrect matrix input.
+	 */
+	public static double[][] readAij(int numStates, BufferedReader br) throws Exception {
+		String[] row;
+		double[] newRow;
+		double[][] aij = new double[numStates][numStates];
+		
+		for (int i = 0; i < numStates; i++) {
+			newRow = new double[numStates];
+			row = br.readLine().split(" ");
+			double sum = 0;
+			if (row.length != numStates) throw new ParserFileException("Error: Number of entries in matrix is incorrect!");
+			for (int j = 0; j < row.length; j++) {
+				newRow[j] = Double.parseDouble(row[j]);
+				sum += newRow[j];
+			}
+			
+			// Check to make sure the sum of the entire row sums up to 1.0
+			if (Math.abs(1.0 - sum) > tolerated_error) {
+				throw new ParserFileException("Error: the sum of row " + i + " did not sum up to 1.0.");
+			}
+			
+			aij[i] = newRow;
+		}
+		
+		// Check to make sure the sum of each column sums up to 1.0
+		for (int j = 0; j < numStates; j++) {
+			double sum = 0;
+			for (int i = 0; i < numStates; i++) {
+				sum+= aij[i][j];
+			}
+			if (Math.abs(1.0 - sum) > tolerated_error) {
+				throw new ParserFileException("Error: the sum of col " + j + " did not sum up to 1.0.");
+			}
+		}
+		return aij;
+	}
+	
+}

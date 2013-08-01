e189d1e406074e56156214a00de18ca8e2296459
kl23 <kl23@94e8b07c-332e-4ac0-aa82-9412fb3d5b0f>
2013-08-01 22:32:11 +0000

    Starting implementation of multivariate optimization heuristic.

---
+++ Code/Java/Unstable/edu/rice/cs/bioinfo/programs/phmm/src/optimize/MultivariateOptimizer.java
@@ -0,0 +1,15 @@
+/**
+ * Performs expectation-maximization (E-M) of
+ * continuous model parameters in PhyloNet-HMM.
+ * E-M iterations use a multivariate optimization heuristic that
+ * incorporates Brent's method for univariate optimization.
+ * Use this approach in lieu of Baum-Welch.
+ */
+
+package optimize;
+
+public class MultivariateOptimizer {
+    public MultivariateOptimizer () {
+	
+    }
+}

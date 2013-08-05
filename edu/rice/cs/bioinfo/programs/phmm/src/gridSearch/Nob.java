f3c51d25b19a4882a57405657e8901a4250c0afe
kt10 <kathy.yvy.truong@gmail.com>
2013-08-05 21:16:48 +0000

    implemented abstract Nob class

--- edu/rice/cs/bioinfo/programs/phmm/src/gridSearch/Nob.java
+++ edu/rice/cs/bioinfo/programs/phmm/src/gridSearch/Nob.java
@@ -4,4 +4,12 @@ public abstract class Nob {
 	public Nob() {
 		
 	}
+	
+	private int g;
+	private double min;
+	private double max;
+	
+	abstract public double[] getSamples();
+	
+	abstract public void set_param(double value);
 }

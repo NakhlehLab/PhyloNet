ceb335ec6f3cdbe5b7096755128fb90dfe51556e
kl23 <kl23@94e8b07c-332e-4ac0-aa82-9412fb3d5b0f>
2013-08-07 22:11:10 +0000

    Forgot to add two files.

---
+++ edu/rice/cs/bioinfo/programs/phmm/src/gridSearch/GeneGenealogyNob.java
@@ -0,0 +1,25 @@
+package gridSearch;
+
+//import phylogeny.Node;
+import edu.rice.cs.bioinfo.programs.phylonet.structs.tree.model.TNode;
+
+public class GeneGenealogyNob extends Nob {
+
+    private TNode childNode;
+
+    public GeneGenealogyNob (int gIn, double minIn, double maxIn, TNode childNodeIn)  {
+        super (gIn, minIn, maxIn);
+        this.childNode = childNodeIn;
+    }
+
+    @Override
+    public void set_param(double value) {
+        childNode.setParentDistance(value);
+    }
+
+    @Override
+    public double get_param() {
+        return childNode.getParentDistance();
+    }
+
+}

934ed36cb8a90ff003c3bc33e581aba85e1d7eb5
kl23 <kl23@94e8b07c-332e-4ac0-aa82-9412fb3d5b0f>
2013-08-07 22:11:10 +0000

    Forgot to add two files.

---
+++ Code/Java/Unstable/edu/rice/cs/bioinfo/programs/phmm/src/gridSearch/ParentalTreeNob.java
@@ -0,0 +1,32 @@
+package gridSearch;
+
+import edu.rice.cs.bioinfo.programs.phylonet.structs.network.Network;
+import edu.rice.cs.bioinfo.programs.phylonet.structs.network.NetNode;
+
+public class ParentalTreeNob extends Nob {
+    // an annoying detail about using Network class to represent trees
+    protected NetNode<Double> parentNode;
+    private NetNode<Double> childNode;
+
+    public ParentalTreeNob(int gIn, double minIn, double maxIn, NetNode<Double> childNodeIn)  {
+        super (gIn, minIn, maxIn);
+        this.childNode = childNodeIn;
+
+	if (childNode.getParentNumber() != 1) {
+	    throw (new RuntimeException("ERROR: ParentalTreeNob accepts only tree nodes with exactly one parent, even though they're represented using NetNode objects. This excludes root nodes."));
+	}
+
+	parentNode = childNode.getParents().iterator().next();
+    }
+
+    @Override
+    public void set_param(double value) {
+        childNode.setParentDistance(parentNode, value);
+    }
+
+    @Override
+    public double get_param() {
+        return childNode.getParentDistance(parentNode);
+    }
+
+}

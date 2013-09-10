5868600f7f160f06b0ca4daa536c6f769f18480c
kl23 <kl23@94e8b07c-332e-4ac0-aa82-9412fb3d5b0f>
2013-09-10 16:05:47 +0000

    Fixed a few bugs with the new informative sites filtering feature and the gene genealogy topological equivalence class modeling feature.

--- edu/rice/cs/bioinfo/programs/phmm/src/util/TreeUtils.java
+++ edu/rice/cs/bioinfo/programs/phmm/src/util/TreeUtils.java
@@ -6,13 +6,20 @@
 
 package util;
 
+import edu.rice.cs.bioinfo.programs.phylonet.structs.tree.model.Tree;
+
 // bleh
 //import edu.rice.cs.bioinfo.programs.phylonet.structs.tree.model.Tree;
 //import gsp.ra.Tree;
 
 public class TreeUtils {
-    public static int calculateRobinsonFouldsDistance (edu.rice.cs.bioinfo.programs.phylonet.structs.tree.model.Tree t1, 
-						       edu.rice.cs.bioinfo.programs.phylonet.structs.tree.model.Tree t2) {
+    /**
+     * WARNING - no safety checks.
+     * Doesn't check to see if both input trees have the same
+     * number of taxa, the same set of taxa, etc.
+     */
+    public static int calculateRobinsonFouldsDistance (Tree t1, 
+						       Tree t2) {
 	gsp.ra.Tree ut1 = new gsp.ra.Tree();
 	gsp.ra.Tree ut2 = new gsp.ra.Tree();
 	// kliu - was toNewickWD - I don't think that was correct
@@ -21,6 +28,16 @@ public class TreeUtils {
 	// work with unrooted versions for Robinson-Foulds calculation
 	ut1.unroot();
 	ut2.unroot();
-	return (ut1.computeRobinsonFouldsDistance(ut2));
+	// warning - Robinson-Foulds calculation only works for unrooted
+	// trees with at least one internal edge
+	// 
+	// fails for less than four taxa
+	if ((ut1.getNumTaxa() < 4) && (ut2.getNumTaxa() < 4)) {
+	    // empty sets are identical by definition
+	    return (0);
+	}
+	else {
+	    return (ut1.computeRobinsonFouldsDistance(ut2));
+	}
     }
 }

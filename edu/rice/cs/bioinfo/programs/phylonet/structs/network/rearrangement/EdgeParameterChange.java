325bab634e8a35209550b2c18c06489441f9eb1a
yy9 <yy9@94e8b07c-332e-4ac0-aa82-9412fb3d5b0f>
2015-05-22 21:23:51 +0000

    Search in the old structure

--- edu/rice/cs/bioinfo/programs/phylonet/structs/network/rearrangement/EdgeParameterChange.java
+++ edu/rice/cs/bioinfo/programs/phylonet/structs/network/rearrangement/EdgeParameterChange.java
@@ -1,11 +1,16 @@
 package edu.rice.cs.bioinfo.programs.phylonet.structs.network.rearrangement;
 
+import edu.rice.cs.bioinfo.library.programming.Tuple;
+import edu.rice.cs.bioinfo.programs.phylonet.structs.network.NetNode;
+import edu.rice.cs.bioinfo.programs.phylonet.structs.network.Network;
+
+import java.util.Random;
+
 /**
  * Created by yunyu on 10/29/14.
  */
 public abstract class EdgeParameterChange extends NetworkRearrangementOperation{
     double _windowSize = 0.1;
-    double _maxBrlen = 6;
 
     public void setWindowSize(double windowSize){
         _windowSize = windowSize;
@@ -13,13 +18,19 @@ public abstract class EdgeParameterChange extends NetworkRearrangementOperation{
 
     protected double drawRandomParameter(double originalValue, double lowerBound, double upperBound){
         double random;
+        lowerBound = Math.max(lowerBound, originalValue - _windowSize);
         upperBound = Math.min(upperBound, originalValue + _windowSize);
-        lowerBound = Math.min( Math.max(lowerBound, originalValue - _windowSize), upperBound - _windowSize);
-        originalValue = Math.min(originalValue, upperBound);
+
+        //System.out.println(originalValue + " in (" + lowerBound + "," + upperBound+")");
         do{
             random = originalValue + (Math.random()*2-1)*_windowSize;
         }while(random<lowerBound || random>upperBound);
+
         return random;
+    }
+
+    public void setParameters(Network network, Tuple<NetNode,NetNode> targetEdge, double targetEdgeBrlen, double targetEdgeInheriProb){
+        super.setParameters(network, targetEdge, targetEdgeBrlen, targetEdgeInheriProb, null, null, null, null, null, null);
     }
 
 }
--- edu/rice/cs/bioinfo/programs/phylonet/structs/network/rearrangement/NetworkNeighbourhoodGenerator.java
+++ edu/rice/cs/bioinfo/programs/phylonet/structs/network/rearrangement/NetworkNeighbourhoodGenerator.java
@@ -5,6 +5,9 @@ package edu.rice.cs.bioinfo.programs.phylonet.structs.network.rearrangement;
 import edu.rice.cs.bioinfo.library.programming.*;
 import edu.rice.cs.bioinfo.programs.phylonet.structs.network.NetNode;
 import edu.rice.cs.bioinfo.programs.phylonet.structs.network.Network;
+import edu.rice.cs.bioinfo.programs.phylonet.structs.network.util.Networks;
+
+import java.util.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,15 +16,81 @@ import edu.rice.cs.bioinfo.programs.phylonet.structs.network.Network;
  * Time: 9:26 PM
  * To change this template use File | Settings | File Templates.
  */
-public interface NetworkNeighbourhoodGenerator{
+public abstract class NetworkNeighbourhoodGenerator{
+    protected boolean _printDetails = true;
 
-   public abstract void performRearrangement(Network network, Integer operation, Tuple<NetNode,NetNode> targetEdge, Tuple<NetNode,NetNode> sourceEdge, Tuple<NetNode,NetNode> destinationEdge);
+    public abstract void undo();
 
-   public abstract void undo();
+    public abstract void mutateNetwork(Network network);
 
+    protected String printEdge(Tuple<NetNode, NetNode> edge) {
+        return "(" + edge.Item1.getName() + "," + edge.Item2.getName() + ")";
+    }
 
-   public abstract void computeRandomNeighbour(Network network);
+    protected boolean isNetworkValid(Network network, Set<String> leaves){
+        int count = 0;
+        for(Object leaf: network.getLeaves()){
+            if(leaves.contains(((NetNode)leaf).getName())){
+                count++;
+            }
+            else{
+                return false;
+            }
+        }
+        if(count!=leaves.size())return false;
+        if(!Networks.isDisconnectedNetwork(network,null))return false;
+        for(Object node: Networks.postTraversal(network)){
+            double totalProb = 0;
+            for (Object parent : ((NetNode) node).getParents()) {
+                totalProb += ((NetNode) node).getParentProbability((NetNode) parent);
+            }
+            if(((NetNode)node).getChildCount()==1 && (((NetNode)node).getParentCount()==1||((NetNode)node).getParentCount()==0)){
+                return false;
+            }
+            if(((NetNode)node).isNetworkNode()){
+                if(Math.abs(totalProb - 1) > 0.00001) {
+                    throw new RuntimeException(network.toString());
+                }
+            }
+            else if(!((NetNode)node).isRoot()){
+                if(!Double.isNaN(totalProb)){
+                    throw new RuntimeException(network.toString());
+                }
+            }
+        }
+        return true;
+    }
 
-    public abstract String getOperationName();
+    protected boolean checkHybrid(Network network, Set<String> requiredHybridSet){
+        if(requiredHybridSet==null) return true;
+        Map<NetNode, Set<NetNode>> node2children = new HashMap<>();
+        Set<NetNode> reticulationNodes = new HashSet<>();
+        for(Object o: Networks.postTraversal(network)){
+            NetNode node = (NetNode)o;
+            Set<NetNode> childrenSet = new HashSet<>();
+            node2children.put(node, childrenSet);
+            if(node.isNetworkNode()){
+                reticulationNodes.add(node);
+            }
+            for(Object child: node.getChildren()){
+                childrenSet.add((NetNode)child);
+                childrenSet.addAll(node2children.get(child));
+            }
+        }
+        Set<String> hybridSet = new HashSet<>();
+        for(NetNode reticulation: reticulationNodes){
+            for(NetNode child: node2children.get(reticulation)){
+                if(child.isLeaf()){
+                    if(!requiredHybridSet.contains(child.getName())){
+                        return false;
+                    }
+                    else{
+                        hybridSet.add(child.getName());
+                    }
+                }
+            }
+        }
+        return hybridSet.size() == requiredHybridSet.size();
+    }
 
 }

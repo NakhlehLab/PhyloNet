6a0d6b54f5123349d6e155074ce44f1e5a2578c4
dw20 <dw20@94e8b07c-332e-4ac0-aa82-9412fb3d5b0f>
2017-01-05 21:22:24 +0000

    fix parameter

--- Code/Java/Unstable/edu/rice/cs/bioinfo/programs/phylonet/algos/MCMCseq/move/all/ScaleAll.java
+++ Code/Java/Unstable/edu/rice/cs/bioinfo/programs/phylonet/algos/MCMCseq/move/all/ScaleAll.java
@@ -13,6 +13,7 @@ import java.util.List;
 /**
  * Created by wendingqiao on 4/29/16.
  * Multiply times of trees and network, population size of network by a random factor
+ * TODO by dw20: doesn't change population sizes due to correlations (cite: MCMCcoal)
  */
 public class ScaleAll extends AllOperator {
 
@@ -26,29 +27,29 @@ public class ScaleAll extends AllOperator {
     public ScaleAll(List<UltrametricTree> trees, UltrametricNetwork net) {
         super(trees, net);
         _lowerLimit = Math.min(_upperLimit,
-                1.0 / Math.exp(50.0 / (trees.size()+1.0) / trees.get(0).getInternalNodes().size()));
-        _scaleFactor = (_upperLimit + _lowerLimit) / 2.0;
+                1.0 / Math.exp(20.0 / (trees.size()+1.0) / trees.get(0).getInternalNodes().size())); // TODO: 50
+        _scaleFactor = _lowerLimit;
     }
 
     @Override
     public double propose() {
         scale = getScaler();
-        int dimension = 1;
-
-        if(Utils._CONST_POP_SIZE) {
-            _edges = null;
-        } else {
-            _edges = Networks.getAllEdges(_speciesNet.getNetwork());
-            dimension += _edges.size();
-        }
-        scalePopSize (scale);
+        int dimension = 0;
+//        int dimension = 1;
+//        if(Utils._CONST_POP_SIZE) {
+//            _edges = null;
+//        } else {
+//            _edges = Networks.getAllEdges(_speciesNet.getNetwork());
+//            dimension += _edges.size();
+//        }
+//        scalePopSize (scale);
 
         dimension += _speciesNet.getInternalNodeCount();
-        scaleHeight(scale);
+        scaleHeight(scale, false);
 
         for (UltrametricTree ut : _allTrees) {
             dimension += ut.getInternalNodes().size();
-            ut.scale(scale);
+            ut.scale(scale, false);
         }
 
         return Math.log(scale) * (dimension - 2);
@@ -56,10 +57,10 @@ public class ScaleAll extends AllOperator {
 
     @Override
     public void undo() {
-        scalePopSize(1.0 / scale);
-        scaleHeight(1.0 / scale);
+//        scalePopSize(1.0 / scale);
+        scaleHeight(1.0 / scale, true);
         for (UltrametricTree ut : _allTrees) {
-            ut.scale(1.0 / scale);
+            ut.scale(1.0 / scale, true);
         }
     }
 
@@ -83,27 +84,32 @@ public class ScaleAll extends AllOperator {
         return _scaleFactor + Randomizer.getRandomDouble() * (1.0 / _scaleFactor - _scaleFactor);
     }
 
-    private void scalePopSize(double scale) {
-        NetNode root = _speciesNet.getNetwork().getRoot();
-        root.setRootPopSize(root.getRootPopSize() * scale);
-        if(Utils._CONST_POP_SIZE) {
-            return;
-        }
-        if(_edges == null) {
-            throw new RuntimeException("not CONST population size!!!!");
-        }
-        for(Tuple<NetNode<NetNodeInfo>, NetNode<NetNodeInfo>> edge : _edges) {
-            edge.Item1.setParentSupport(edge.Item2, edge.Item1.getParentSupport(edge.Item2) * scale);
-        }
-    }
+//    private void scalePopSize(double scale) {
+//        NetNode root = _speciesNet.getNetwork().getRoot();
+//        root.setRootPopSize(root.getRootPopSize() * scale);
+//        if(Utils._CONST_POP_SIZE) {
+//            return;
+//        }
+//        if(_edges == null) {
+//            throw new RuntimeException("not CONST population size!!!!");
+//        }
+//        for(Tuple<NetNode<NetNodeInfo>, NetNode<NetNodeInfo>> edge : _edges) {
+//            edge.Item1.setParentSupport(edge.Item2, edge.Item1.getParentSupport(edge.Item2) * scale);
+//        }
+//    }
 
-    private void scaleHeight(double scale) {
+    private void scaleHeight(double scale, boolean undo) {
         for(NetNode<NetNodeInfo> node : Networks.postTraversal(_speciesNet.getNetwork())) {
             if(node.isLeaf()) continue;
-            double newH = node.getData().getHeight() * scale;
-            node.getData().setHeight(newH);
+            if(undo) {
+                node.getData().recoverHeight();
+            } else {
+                node.getData().storeHeight();
+                double newH = node.getData().getHeight() * scale;
+                node.getData().setHeight(newH);
+            }
             for(NetNode<NetNodeInfo> child : node.getChildren()) {
-                child.setParentDistance(node, newH - child.getData().getHeight());
+                child.setParentDistance(node, node.getData().getHeight() - child.getData().getHeight());
             }
         }
     }

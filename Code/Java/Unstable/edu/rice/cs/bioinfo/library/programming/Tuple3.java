14861b933f21f9edd89da793175966665e14e957
jz55 <jz55@94e8b07c-332e-4ac0-aa82-9412fb3d5b0f>
2018-03-08 18:09:26 +0000

    update

--- Code/Java/Unstable/edu/rice/cs/bioinfo/library/programming/Tuple3.java
+++ Code/Java/Unstable/edu/rice/cs/bioinfo/library/programming/Tuple3.java
@@ -16,4 +16,37 @@ public class Tuple3<T1,T2,T3> extends Tuple<T1,T2>
         super(item1, item2);
         Item3 = item3;
     }
+
+    @Override
+    public boolean equals(Object candidate)
+    {
+        try
+        {
+            return equals((Tuple3<T1,T2,T3>)candidate);
+        }
+        catch (ClassCastException e)
+        {
+            return false;
+        }
+    }
+
+    public boolean equals(Tuple3<T1,T2,T3> candidate)
+    {
+        if(candidate == null)
+            return false;
+
+        return candidate.Item1.equals(Item1) && candidate.Item2.equals(Item2) && candidate.Item3.equals(Item3);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Item1.hashCode() ^ Item2.hashCode() ^ Item3.hashCode();
+    }
+
+    @Override
+    public String toString()
+    {
+        return "(" + Item1 + ", " + Item2 + ", " + Item3 + ")";
+    }
 }
